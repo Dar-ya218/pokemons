b3526788ef5f6529594e9913a8ceea0537fde3b7
Dar-ya218 <dasharoura13465@gmail.com>
2024-07-18 17:26:57 +0200

    Style elements

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ interface PokemonDetails {
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
   const [pokemonDetails, setPokemonDetails] = useState<PokemonDetails[]>([])
+  const [pokemonFiltered, setPokemonFiltered] = useState<PokemonDetails[]>([])
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10')
@@ -47,24 +48,28 @@ function App() {
   return (
     <> 
       <h2>Fire Pokemon filtered</h2>
-      {
-        pokemonDetails.length > 0 && pokemonDetails.filter((pokemon) => pokemon.types[0]?.type.name === 'fire').sort((a, b) => a.id - b.id).map((pokemon) => (
-          <div key={pokemon.id} className="pokemon">
-            <h2>{pokemon.name}</h2>
-            <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
-            <p>{pokemon.types.map((type) => type.type.name).join(', ')}</p>
-          </div>
-        ))
-      }
+      <section>
+        {
+          pokemonDetails.length > 0 && pokemonDetails.filter((pokemon) => pokemon.types[0]?.type.name === 'fire').sort((a, b) => a.id - b.id).map((pokemon) => (
+            <div key={pokemon.id} className="card">
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
+              <p>{pokemon.types.map((type) => type.type.name).join(', ')}</p>
+            </div>
+          ))
+        }
+      </section>
       <h2>First 10 pokemons</h2>
-      {
-        pokemonDetails.length > 0 && pokemonDetails.map((poke: PokemonDetails) => (
-          <div key={poke.id}>
-            <h1>{poke.name}</h1>
-            <img src={poke.sprites.front_shiny} alt={poke.name} />
-          </div>
-        ))
-      }
+      <section>
+        {
+          pokemonDetails.length > 0 && pokemonDetails.map((poke: PokemonDetails) => (
+            <div key={poke.id} className='card'>
+              <h1>{poke.name}</h1>
+              <img src={poke.sprites.front_shiny} alt={poke.name} />
+            </div>
+          ))
+        }
+      </section>
     </>
   )
 }
